6e209e89989992ea6d66c96d7d4f16d64ce2d482
kyh5392 <kyh90495392@gmail.com>
2016-08-24 18:06:11 +0900

    레이아웃 작성 중...

--- index.php
+++ index.php
@@ -1,1 +1,112 @@
-<h1>Hellow world</h1>
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>GD홈라이스</title>
+        <!-- Bootstrap -->
+        <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/assets/css/default.css" rel="stylesheet">
+        <link href="/assets/css/main.css" rel="stylesheet">
+
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+        <a href="#gnb" class="skip-nav sr-only sr-only-focusable">주메뉴로 건너뛰기</a>
+        <a href="#content" class="skip-nav sr-only sr-only-focusable">컨텐츠로 건너뛰기</a>
+
+        <header id="header">
+            <div class="container">
+                <h1><a href="index.php"><img src="/assets/images/logo.jpg" alt="회사로고"></a></h1>
+                <ul class="member">
+                    <li>
+                        <a href="#">로그인</a>
+                    </li>
+                    <li>
+                        <a href="#">회원가입</a>
+                    </li>
+                    <li>
+                        <a href="#">마이페이지</a>
+                    </li>
+                    <li>
+                        <a href="#">장바구니</a>
+                    </li>
+                </ul>
+                <div class="search-area">
+					<form>
+						<div class="form-group">
+							<label for="search-keyword" class="sr-only">검색어</label>
+							<input id="search-keyword" type="text" class="form-control" placeholder="검색어를 입력하세요.">
+						</div>
+						<button type="submit" class="btn">
+						    <span>
+						        <img src="/assets/images/sub/search.png" alt="검색버튼 이미지">
+						    </span>
+						</button>
+					</form>
+				</div>
+            </div>
+        </header>
+        <nav id="gnb" tabindex="-1">
+            <div class="wrapper">
+                <div class="container">
+                    <ul>
+                        <li>
+                            <a href="#">브랜드스토리</a>
+                            <ul>
+                                <li><a href="#">메뉴1</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">매장정보</a>
+                            <ul>
+                                <li><a href="#">메뉴1</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">파티메뉴</a>
+                            <ul>
+                                <li><a href="#">메뉴1</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">단품메뉴</a>
+                            <ul>
+                                <li><a href="#">메뉴1</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">이용안내</a>
+                            <ul>
+                                <li><a href="#">메뉴1</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">커뮤니티</a>
+                            <ul>
+                                <li><a href="#">메뉴1</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <main id="content" tabindex="-1">
+
+        </main>
+        <nav id="nav-foot">
+
+        </nav>
+        <footer id="footer">
+
+        </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    </body>
+</html>
