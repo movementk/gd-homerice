ac1c3443a04ff01b876bb818337c04a931b8c98c
kyh5392 <kyh90495392@gmail.com>
2016-08-28 21:04:37 +0900

    [web] menu-list 수정

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
             </ul>
         </div>
 
-        <div class="introduce">
+        <div id="introduce">
             <div class="container">
                 <div class="store">
                     <h3>Store</h3>
@@ -56,7 +56,24 @@
                 </div>
             </div>
         </div>
-        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/sub/main_menu.php'); ?>
+        <div id="main-menu">
+            <section class="party-menu">
+                <div class="container">
+                    <h4>Home Party</h4>
+                    <h3>고품격 요리 홈파티 메뉴</h3>
+                    <p>생일, 모임, 돌잔치, 캠핑 등 스페셜 푸드가 필요한 날엔 홈파티 메뉴로 즐겨보세요</p>
+                    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/sub/party_menu.php'); ?>
+                </div>
+            </section>
+            <section class="single-menu">
+                <div class="container">
+                    <h4>Single Dish</h4>
+                    <h3>간편하게 즐기는 단품메뉴</h3>
+                    <p>야유회, 운동회, 세미나 등 간편한 푸드가 필요한 날엔 단품메뉴로 즐겨보세요</p>
+                    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/sub/single_menu.php'); ?>
+                </div>
+            </section>
+        </div>
     </main>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
---
+++ sub/single_menu.php
@@ -0,0 +1,176 @@
+<ul class="menu-list row">
+    <li class="col-xs-4">
+        <div class="menu-info">
+            <a href="#">
+                <p class="menu-img">
+                    <img src="/assets/images/sub/menu_img04.gif" alt="음식 이미지">
+                </p>
+            </a>
+            <div class="details">
+                <h5>
+                    <a href="#">
+                        <small>신선한 일식 대표 메뉴를 한번에 즐길 수 있는 구성</small><br>
+                        일식단품(1인분)
+                    </a>
+                </h5>
+                <p class="amount">
+                    <strong>19,900</strong>
+                    <small>25,000원</small>
+                </p>
+                <p class="icon">
+                    <a href="#" class="cart">
+                        <img src="/assets/images/sub/cart.png" alt="장바구니 버튼">
+                    </a>
+                    <a href="#" class="interest">
+                        <img src="/assets/images/sub/interest.png" alt="관심상품 버튼">
+                    </a>
+                </p>
+            </div>
+        </div>
+    </li>
+    <li class="col-xs-4">
+        <div class="menu-info">
+            <a href="#">
+                <p class="menu-img">
+                    <img src="/assets/images/sub/menu_img05.gif" alt="음식 이미지">
+                </p>
+            </a>
+            <div class="details">
+                <h5>
+                    <a href="#">
+                        <small>다양한 식감으로 뱃속 든든하게 채워주는 양식</small><br>
+                        양식단품(1인분)
+                    </a>
+                </h5>
+                <p class="amount">
+                    <strong>15,900</strong>
+                    <small>18,000원</small>
+                </p>
+                <p class="icon">
+                    <a href="#" class="cart">
+                        <img src="/assets/images/sub/cart.png" alt="장바구니 버튼">
+                    </a>
+                    <a href="#" class="interest">
+                        <img src="/assets/images/sub/interest.png" alt="관심상품 버튼">
+                    </a>
+                </p>
+            </div>
+        </div>
+    </li>
+    <li class="col-xs-4">
+        <div class="menu-info">
+            <a href="#">
+                <p class="menu-img">
+                    <img src="/assets/images/sub/menu_img06.gif" alt="음식 이미지">
+                </p>
+            </a>
+            <div class="details">
+                <h5>
+                    <a href="#">
+                        <small>한국 전통의 맛을 살려 하나 하나 정갈하게 담아낸 한식 메뉴</small><br>
+                        한식단품(1인분)
+                    </a>
+                </h5>
+                <p class="amount">
+                    <strong>15,900</strong>
+                    <small>18,000원</small>
+                </p>
+                <p class="icon">
+                    <a href="#" class="cart">
+                        <img src="/assets/images/sub/cart.png" alt="장바구니 버튼">
+                    </a>
+                    <a href="#" class="interest">
+                        <img src="/assets/images/sub/interest.png" alt="관심상품 버튼">
+                    </a>
+                </p>
+            </div>
+        </div>
+    </li>
+    <li class="col-xs-4">
+        <div class="menu-info">
+            <a href="#">
+                <p class="menu-img">
+                    <img src="/assets/images/sub/menu_img04.gif" alt="음식 이미지">
+                </p>
+            </a>
+            <div class="details">
+                <h5>
+                    <a href="#">
+                        <small>신선한 일식 대표 메뉴를 한번에 즐길 수 있는 구성</small><br>
+                        일식단품(1인분)
+                    </a>
+                </h5>
+                <p class="amount">
+                    <strong>19,900</strong>
+                    <small>25,000원</small>
+                </p>
+                <p class="icon">
+                    <a href="#" class="cart">
+                        <img src="/assets/images/sub/cart.png" alt="장바구니 버튼">
+                    </a>
+                    <a href="#" class="interest">
+                        <img src="/assets/images/sub/interest.png" alt="관심상품 버튼">
+                    </a>
+                </p>
+            </div>
+        </div>
+    </li>
+    <li class="col-xs-4">
+        <div class="menu-info">
+            <a href="#">
+                <p class="menu-img">
+                    <img src="/assets/images/sub/menu_img05.gif" alt="음식 이미지">
+                </p>
+            </a>
+            <div class="details">
+                <h5>
+                    <a href="#">
+                        <small>다양한 식감으로 뱃속 든든하게 채워주는 양식</small><br>
+                        양식단품(1인분)
+                    </a>
+                </h5>
+                <p class="amount">
+                    <strong>15,900</strong>
+                    <small>18,000원</small>
+                </p>
+                <p class="icon">
+                    <a href="#" class="cart">
+                        <img src="/assets/images/sub/cart.png" alt="장바구니 버튼">
+                    </a>
+                    <a href="#" class="interest">
+                        <img src="/assets/images/sub/interest.png" alt="관심상품 버튼">
+                    </a>
+                </p>
+            </div>
+        </div>
+    </li>
+    <li class="col-xs-4">
+        <div class="menu-info">
+            <a href="#">
+                <p class="menu-img">
+                    <img src="/assets/images/sub/menu_img06.gif" alt="음식 이미지">
+                </p>
+            </a>
+            <div class="details">
+                <h5>
+                    <a href="#">
+                        <small>한국 전통의 맛을 살려 하나 하나 정갈하게 담아낸 한식 메뉴</small><br>
+                        한식단품(1인분)
+                    </a>
+                </h5>
+                <p class="amount">
+                    <strong>15,900</strong>
+                    <small>18,000원</small>
+                </p>
+                <p class="icon">
+                    <a href="#" class="cart">
+                        <img src="/assets/images/sub/cart.png" alt="장바구니 버튼">
+                    </a>
+                    <a href="#" class="interest">
+                        <img src="/assets/images/sub/interest.png" alt="관심상품 버튼">
+                    </a>
+                </p>
+            </div>
+        </div>
+    </li>
+</ul>
